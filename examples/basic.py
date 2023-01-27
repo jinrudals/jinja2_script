325851ac32da2651f934fe5319b8fd7b53e7a08b
Benjamin Jin <benjamin.jin@semifive.com>
2023-01-27 17:40:10 +0900

    Add example files

---
+++ examples/basic.py
@@ -0,0 +1,24 @@
+from jinja_script_block import ScriptBlockExtension
+from jinja2 import Environment
+
+env = Environment(extensions=[ScriptBlockExtension])
+
+template = env.from_string('''
+  {%- script myblock %}
+  containers = []
+  value = 333
+  {% endscript -%}
+
+  {%- script myblock2 %}
+  def add(obj):
+    obj.append(3)
+  {% endscript -%}
+
+{%- set _=myblock.containers.append('1') -%}
+{%- set _=myblock2.add(myblock.containers) -%}
+
+{{myblock.containers}}
+{{myblock.value}}
+''')
+rendered = template.render()
+print(rendered)
